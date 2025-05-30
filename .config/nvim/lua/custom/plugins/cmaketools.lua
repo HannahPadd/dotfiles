1af720813e4bff53f888d9b04b32267ad7180d47
HannahPadd <hannahlindrob@outlook.com>
2025-05-30 15:46:07 +0200

    Epic neovim update

--- .config/nvim/lua/custom/plugins/cmaketools.lua
+++ .config/nvim/lua/custom/plugins/cmaketools.lua
@@ -1,6 +1,34 @@
 return {
-	"Civitasv/cmake-tools.nvim",
-	opts = {
-		cmake_terminal = "toggleterm", -- 🔥 This enables native Toggleterm support
+	{
+		"Civitasv/cmake-tools.nvim",
+		opts = {
+			cmake_terminal = "toggleterm", -- still safe to leave
+
+			cmake_executor = {
+				name = "toggleterm",
+				opts = {},
+				default_opts = {
+					toggleterm = {
+						direction = "float", -- floating terminal
+						close_on_exit = false,
+						auto_scroll = true,
+						singleton = true,
+					},
+				},
+			},
+
+			cmake_runner = {
+				name = "toggleterm",
+				opts = {},
+				default_opts = {
+					toggleterm = {
+						direction = "float", -- floating terminal
+						close_on_exit = false,
+						auto_scroll = true,
+						singleton = true,
+					},
+				},
+			},
+		},
 	},
 }
